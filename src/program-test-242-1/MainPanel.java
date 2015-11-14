ab45492a95a8a5ac714ed57d5de1c1a4c8dcb718
faganfv <faganfv@gmail.com>
2015-11-14 18:02:58 -0500

    Fixed uncaught exception after recompiling MainPanel

--- src/program-test-242-1/MainPanel.java
+++ src/program-test-242-1/MainPanel.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.*;
+import javax.swing.text.BadLocationException;
 
 public class MainPanel extends JPanel{
     
@@ -188,7 +189,11 @@ public class MainPanel extends JPanel{
 				argWriter(argText);
 				configItems.setTestInputs(inputsField.getText());
                 OutputFrame batchFrame = new OutputFrame(true);
-				batchFrame.batchPanel.displayStudentOutputs(batchDirectory, batchStudents);
+				try {
+					batchFrame.batchPanel.displayStudentOutputs(batchDirectory, batchStudents);
+				} catch (BadLocationException ex) {
+					Logger.getLogger(MainPanel.class.getName()).log(Level.SEVERE, null, ex);
+				}
 				batchTest.main(null);
             }
         });
